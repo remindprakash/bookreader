e818340b58dd441b9addb5c2090eb6d2a29dfeec
Gnanaprakash Ramswami <remindprakash@hotmail.com>
2014-05-04 16:43:38 -0700

    initial setup

---
+++ src/main/java/com/bookreader/app/main/ApplicationStartup.java
@@ -0,0 +1,9 @@
+package com.bookreader.app.main;
+
+public class ApplicationStartup {
+	
+	public static void main(String[] args) {
+		System.out.println("Book reader application started");
+	}
+
+}
