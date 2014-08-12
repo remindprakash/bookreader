f34f660c7b7d32543ab147988e1553c55831a930
Gnanaprakash Ramswami <remindprakash@hotmail.com>
2014-08-11 23:24:50 -0700

    ebook reader app code initial version

--- src/main/java/com/bookreader/app/main/ApplicationStartup.java
+++ src/main/java/com/bookreader/app/main/ApplicationStartup.java
@@ -1,9 +1,27 @@
 package com.bookreader.app.main;
 
-public class ApplicationStartup {
-	
-	public static void main(String[] args) {
-		System.out.println("Book reader application started");
-	}
+import javax.swing.JOptionPane;
 
-}
+import com.bookreader.userInterface.MainUI;
+//import com.bookreader.ui.EBRApp;
+
+public class ApplicationStartup 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println("Book reader application started");
+		
+	    try
+	    {
+	    	//EBRApp frame=new EBRApp();
+	    	//frame.initReader();
+	    	
+	    	MainUI frame = new MainUI();
+	    	frame.initiateApplication();
+	    	
+	    }
+	  catch(Exception e)
+	    {
+	        JOptionPane.showMessageDialog(null, e.getMessage());}
+	  }
+	}
