469013a67fe64d3290c8d871ddbacb8bd1a79213
bharathkumar.feb14@gmail.com <bharath214>
2014-12-12 12:33:09 +0530

    add epub file

--- src/main/java/com/bookreader/ui/EpubUI.java
+++ src/main/java/com/bookreader/ui/EpubUI.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import nl.siegmann.epublib.browsersupport.Navigator;
 import nl.siegmann.epublib.domain.Book;
@@ -19,12 +20,17 @@ import com.bookreader.epub.viewer.ContentPane;
 import com.bookreader.epub.viewer.ToolBar;
 
 public class EpubUI {
+	
+	private static final long serialVersionUID = 1L;
+
+
 	static final Logger log = LoggerFactory.getLogger(EpubUI.class);
 	
 	private Navigator navigator = new Navigator();
 	private BrowseBar browseBar;
 	private ToolBar toolBar;
-	
+
+
 	public JPanel Viewer(InputStream bookStream) {
 		
 		Book book;
@@ -32,7 +38,16 @@ public class EpubUI {
 			book = (new EpubReader()).readEpub(bookStream);
 			FontLoader.loadFontFromBook(book);
 			navigator.gotoBook(book, this);
-		
+			return createMainWindow(navigator);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+		return null;
+	}
+	
+	
+	private JPanel createMainWindow(Navigator navigator) {
+		try{
 			ContentPane htmlPane = new ContentPane(navigator);
 			JPanel contentPanel = new JPanel(new BorderLayout());
 			
@@ -45,12 +60,18 @@ public class EpubUI {
 			//SpineSlider and ButtonBar
 			contentPanel.add(browseBar, BorderLayout.SOUTH);
 			
-			
 			return contentPanel;
-		} catch (IOException e) {
+			
+		}
+		catch(Exception e){
 			log.error(e.getMessage(), e);
 		}
 		return null;
 	}
+	
+	
+	
+	
+	
 
 }
